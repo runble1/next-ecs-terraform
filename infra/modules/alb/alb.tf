8bb4d802756a0ad8f0635adbd4881ec805ed5ec4
runble1 <nishiyama-wataru@dmm.com>
2023-05-03 16:44:19 +0900

    a

--- infra/modules/alb/alb.tf
+++ infra/modules/alb/alb.tf
@@ -198,3 +198,19 @@ resource "aws_security_group" "alb55" {
     Name = "${var.env}-${var.service}-alb55-sg"
   }
 }
+
+
+resource "aws_security_group" "alb56" {
+  name   = "${var.env}-${var.service}-alb56-sg"
+  vpc_id = var.vpc_id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-${var.service}-alb56-sg"
+  }
+}
